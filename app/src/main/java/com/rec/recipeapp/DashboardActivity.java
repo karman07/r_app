6e68bc345840fb68792e2873f75be224092caee7
karman07 <karmansingharora01@gmail.com>
2022-02-23 13:05:21 +0530

    Bottom Navigation

---
+++ app/src/main/java/com/rec/recipeapp/DashboardActivity.java
@@ -0,0 +1,57 @@
+package com.rec.recipeapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeScreen()).commit();
+        }
+
+
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.home:
+                            selectedFragment = new HomeScreen();
+                            break;
+                        case R.id.search:
+                            selectedFragment = new SearchScreen();
+                            break;
+                        case R.id.add:
+                            selectedFragment = new AddScreen();
+                            break;
+                        case R.id.menu:
+                            selectedFragment = new MenuScreen();
+                            break;
+                        case R.id.profile:
+                            selectedFragment= new ProfileScreen();
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+
+            };
+
+}
